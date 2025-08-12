f24fec4447ae0dd52aadbf10b216ef9ad4d017b8
Rahul <rahulrrao@gmail.com>
2025-08-12 10:32:13 +0930

    Fix RestaurantRoutes import paths

--- components/RestaurantRoutes.tsx
+++ components/RestaurantRoutes.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import RestaurantLanding from '../pages/restaurant/RestaurantLanding';
-import RestaurantSignUp from '../pages/restaurant/RestaurantSignUp';
-import RestaurantLogin from '../pages/restaurant/RestaurantLogin';
+import RestaurantLanding from '../pages/restaurants/RestaurantLanding';
+import RestaurantSignUp from '../pages/restaurants/RestaurantSignUp';
+import RestaurantLogin from '../pages/restaurants/RestaurantLogin';
 
 // Placeholder components - we'll create these next
 const RestaurantDashboard = () => <div>Restaurant Dashboard Page</div>;
