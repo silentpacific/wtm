91d66ec25d31f78df8f1f39b23e2bb464fd7a184
Rahul <rahulrrao@gmail.com>
2025-09-01 16:33:13 +0930

    variants

--- src/pages/DemosPage.tsx
+++ src/pages/DemosPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { supabase } from "../supabaseClient"; // adjust path if needed
+import { supabase } from "../services/supabaseClient";
 
 type Menu = {
   id: string;
